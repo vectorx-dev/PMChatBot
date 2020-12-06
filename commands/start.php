f9ef3399d1e980c082653a5cd29e493bfc717028
Vector X <74726477+vectorx-dev@users.noreply.github.com>
2020-12-06 23:03:47 +0530

    More Commands Available Soon

--- commands/start.php
+++ commands/start.php
@@ -3,6 +3,3 @@
 $Bot->command("/\/start/", function ($Update, $Match) use ($Bot, $LANG) {
     $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "parse_mode" => "markdown", "text" => $LANG["START"]]);
 });
-
-
-Sed wait lel high-voltage 
