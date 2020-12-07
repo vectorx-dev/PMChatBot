da662bd2373928818ca2896bd6839c4c6014921f
Vector X <74726477+vectorx-dev@users.noreply.github.com>
2020-12-07 21:24:43 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -22,6 +22,7 @@ if (file_exists(__DIR__ . '/language/default.json')) {
 
 if (empty(getenv("ADMIN_ID"))) {
     echo "Please give admin id";
+    $Admin = 11111111;
 } else {
     $Admin = getenv("ADMIN_ID");
 }
