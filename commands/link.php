fafa885f6ef16091102635a130bbdf359f158445
Vector X <74726477+vectorx-dev@users.noreply.github.com>
2020-12-07 21:56:07 +0530

    Updated Successfully

--- commands/link.php
+++ commands/link.php
@@ -19,3 +19,6 @@ $Bot->command("/\/link/", function ($Update, $Match) use ($Bot, $db, $tur, $LANG
         $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "reply_to_message_id" => $Update["message"]["message_id"], "parse_mode" => "markdown", "text" => $LANG["ERROR_NOTFOUND"]]); 
         return;
     }
+        
+    $Bot->sendMessage(["chat_id" => $Update["message"]["chat"]["id"], "parse_mode" => "markdown", "text" => "*Here is the user you are looking for: * [". $ fid [" fname "]. "](tg: // user? id = ". $ fid [" fid "]. ")\n*Username:* @". $fid[" fusername "]]);});" => $Update["message"]["chat"]["id"], "parse_mode" => "markdown", "text" => "*İşte aradığınız kullanıcı:* [" . $fid["fname"] . "](tg://user?id=" . $fid["fid"] . ")\n*Kullanıcı adı:* @" . $fid["fusername"]]);
+});
